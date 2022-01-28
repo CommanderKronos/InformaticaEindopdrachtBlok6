60ad55e08c8f3ffd588276dbf8808fac3c70abbf
Yuri Wit <yuriwit0@gmail.com>
2022-01-28 12:51:51 +0100

    Added docstrings, finished

--- src/FART.java
+++ src/FART.java
@@ -24,6 +24,9 @@ public class FART{
     private final ArrayList<String> markedArray = new ArrayList<>();
     private static HashMap<String, StringBuilder> contentMap = new HashMap<>();
 
+    /**
+     * This is the constructor of the application
+     */
     public FART() {
 
         JFileChooser j = new JFileChooser();
@@ -32,36 +35,44 @@ public class FART{
         File[] selectedFiles = j.getSelectedFiles();
         if (returnVal == JFileChooser.APPROVE_OPTION) {
 
+            // Read the selected files and store the content
             contentMap = readFiles(selectedFiles);
-            String[] files = contentMap.keySet().toArray(new String[0]);
 
-            explorerList.setListData(files);
+            // Load the array of keys into the left JList object
+            explorerList.setListData(contentMap.keySet().toArray(new String[0]));
             explorerList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-            explorerList.addListSelectionListener(f -> {
-                if (!explorerList.getValueIsAdjusting()) {
-                    String selectedValue = explorerList.getSelectedValue();
-                    displayFile(contentMap.get(selectedValue));
-                    fileArea.setCaretPosition(0);
-                }
-            });
 
         } else {
             JOptionPane.showMessageDialog(mainPanel, "Selecteer tenminste 1 bestand.");
             System.exit(0);
         }
 
+        explorerList.addListSelectionListener(f -> {
+            // This check is needed because a JList's value adjusts twice when the user selects something, this check
+            // prevents events from happening while the value is still being adjusted.
+            if (!explorerList.getValueIsAdjusting()) {
+                String selectedValue = explorerList.getSelectedValue();
+                displayFile(contentMap.get(selectedValue));
+                // The setCaretPosition function is called to set the scroll position of the middle text field
+                // to the top after the content has been loaded.
+                fileArea.setCaretPosition(0);
+            }
+        });
+
         selectButton.addActionListener(e -> {
             String selectedValue = explorerList.getSelectedValue();
+            // Check to see if the selected value in the JList isn't already in the array of selected keys
             if (!markedArray.contains(selectedValue)) {
                 markedArray.add(selectedValue);
+                // A temporary object has to be made to prevent index exceptions
                 Object[] tempObj = markedArray.toArray();
                 selectedList.setListData(Arrays.copyOf(tempObj, tempObj.length, String[].class));
             }
         });
 
         deselectButton.addActionListener(e -> {
-           String selectedValue = explorerList.getSelectedValue();
-           markedArray.remove(selectedValue);
+           markedArray.remove(explorerList.getSelectedValue());
+           // A temporary object has to be made to prevent index exceptions
            Object[] tempObj = markedArray.toArray();
            selectedList.setListData(Arrays.copyOf(tempObj, tempObj.length, String[].class));
         });
@@ -79,6 +90,12 @@ public class FART{
         });
     }
 
+    /**
+     * This function writes the user-selected data to a user-selected file
+     *
+     * @param writeToFile the user-selected file where the data must be written
+     * @throws IOException throws an error if something goes wrong while writing
+     */
     public void writeFile(File writeToFile) throws IOException {
         FileWriter writer = new FileWriter(writeToFile);
         BufferedWriter buffWriter = new BufferedWriter(writer);
@@ -90,10 +107,22 @@ public class FART{
         JOptionPane.showMessageDialog(mainPanel, "Data geschreven naar bestand");
     }
 
+    /**
+     * This function will display the content of the file, once the user selects it in the list.
+     *
+     * @param content The content of the selected file in the application
+     */
     public void displayFile(StringBuilder content) {
         fileArea.setText(String.valueOf(content));
     }
 
+    /**
+     * This function reads all the files selected by the user, then stores all necessary information in a HashMap
+     *
+     * @param selectedFiles, an array of File objects containing all the files the user selected
+     * @return contentMap, a HashMap<String, StringBuilder> containing the DEFINITION in the selected files as keys,
+     * and the content of the files as values.
+     */
     public static HashMap<String, StringBuilder> readFiles(File[] selectedFiles) {
         contentMap = new HashMap<>();
         StringBuilder definition = new StringBuilder();
@@ -143,11 +172,10 @@ public class FART{
         }
 
         JFrame frame = new JFrame("FART - Forensic Application for Reading Text");
-        frame.setContentPane(new FART().mainPanel);;
+        frame.setContentPane(new FART().mainPanel);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
 
-        // TODO: optimaliseer
     }
 }
